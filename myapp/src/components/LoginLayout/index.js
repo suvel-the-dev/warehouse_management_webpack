e800445288117e1abebed7ac89a0bdbc3b962848
Suvel <suvelrathneswar08@gmail.com>
2021-08-05 19:04:13 +0530

    Refectored to ignore background-image

--- myapp/src/components/LoginLayout/index.js
+++ myapp/src/components/LoginLayout/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import Input from '../Input'
 import './style.css'
+import bg_img from '../../asset/bg_img/istockphoto-893494012-1024x1024 1.png'
+import brand_img from '../../asset/band/logo-md.png'
 const LoginLayout = ({ handelLogin }) => {
     return (
         <div className="container">
-            <div className="main-area">
+            <div
+                className="main-area"
+                style={{
+                    backgroundImage: `url(${bg_img})`
+                }}
+            >
                 <div className='login-form-container'>
-                    <div className='brand-logo-md' />
+                    <div
+                        className='brand-logo-md'
+                        style={{
+                            backgroundImage: `url(${brand_img})`
+                        }}
+                    />
                     <div className='login-forms'>
                         <Input
                             label={"USERNAME"}
--- myapp/src/components/Spinner/index.js
+++ myapp/src/components/Spinner/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import './style.css'
+import spinner_icon from '../../asset/band/logo.png'
 const Spinner = () => {
     return (
         <div className='spinner__bg'>
-            <div className="spinner">
+            <div
+                className="spinner"
+                style={{
+                    backgroundImage: `url(${spinner_icon})`
+                }}
+            >
             </div>
             <strong>Warehouse is being rendered, please wait...</strong>
         </div>
